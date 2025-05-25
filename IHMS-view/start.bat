f7ca81c8aadf689bf4e06440998df1e2d44e4e31
BroRem <2052619274@qq.com>
2025-05-25 18:11:29 +0800

    修复启动问题，添加启动脚本 启动方式：npm run dev:compatible

---
+++ IHMS-view/start.bat
@@ -0,0 +1,4 @@
+@echo off
+SET NODE_OPTIONS=--openssl-legacy-provider
+echo 正在启动IHMS前端项目...
+npm run dev 
---
+++ IHMS-view/start.ps1
@@ -0,0 +1,3 @@
+$env:NODE_OPTIONS="--openssl-legacy-provider"
+Write-Host "正在启动IHMS前端项目..." -ForegroundColor Green
+npm run dev 
